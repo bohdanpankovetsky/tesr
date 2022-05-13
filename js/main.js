af1d090a800f1687ae25afe03976a23f84d760dd
bohdanpankovetsky <33544747+bohdanpankovetsky@users.noreply.github.com>
2022-05-13 14:37:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+  "use strict";
+
+  //faq
+  const btns = document.querySelectorAll(".faq-question");
+  function accordion() {
+    this.classList.toggle("open");
+  }
+  btns.forEach((el) => el.addEventListener("click", accordion));
+
+  //menu
+  const menuBtn = document.querySelectorAll(".header-menu-btn");
+  const headerMenu = document.getElementsByClassName("header-menu")[0];
+  function menuBtnToggle() {
+    this.classList.toggle("active");
+    headerMenu.classList.toggle("active");
+    document.getElementsByTagName("html")[0].classList.toggle("overflow");
+  }
+  function menuItemToggle() {
+    menuBtn.forEach((el) => el.classList.toggle("active"));
+    headerMenu.classList.toggle("active");
+    document.getElementsByTagName("html")[0].classList.toggle("overflow");
+  }
+  menuBtn.forEach((el) => el.addEventListener("click", menuBtnToggle));
+  headerMenu.querySelectorAll("a").forEach((el) => el.addEventListener("click", menuItemToggle));
+
+
+  //numbers
+  function increaseNum() {
+    var coords = document.getElementById("numbers").getBoundingClientRect();
+    var windowHeight = document.getElementsByTagName("html")[0].getBoundingClientRect(); //-1200
+    var countH = windowHeight.top + (-windowHeight.top) + (-windowHeight.top);
+    if (countH + 200 >= coords.top) {
+      function counter(id, start, end, duration) {
+        if (end % 1 != 0) {
+          let obj = document.getElementById(id);
+          let current = +start;
+          let range = +end - start;
+          let increment = +end > start ? 0.1 : -0.1;
+          let step = duration / range;
+          let timer = setInterval(() => {
+              current += increment;
+              obj.textContent = current.toFixed(1);
+              if (current == +end) {
+                clearInterval(timer);
+              }
+            }, step);
+        }
+        else {
+          let obj = document.getElementById(id);
+          let current = start;
+          let range = end - start;
+          let increment = end > start ? 1 : -1;
+          let step = Math.abs(Math.floor(duration / range));
+          let timer = setInterval(() => {
+              current += increment;
+              obj.textContent = current;
+              if (current == end) {
+                clearInterval(timer);
+              }
+            }, step);
+        }
+      }
+      counter("number1", 0, document.getElementById('number1').textContent, 3000);
+      counter("number2", 0, document.getElementById('number2').textContent, 3000);
+      counter("number3", 0.0, document.getElementById('number3').textContent, 500);
+      counter("number4", 0, document.getElementById('number4').textContent, 500);
+      document.removeEventListener('scroll', increaseNum);
+    }
+  }
+  document.addEventListener('scroll', increaseNum);
